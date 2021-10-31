b3dde873600a4dc030f1092eb03e0d5b16d6c167
greg <bim.go.consulting@gmail.com>
2021-10-31 14:00:26 +0100

    Add Wall Text

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-import{ IfcAPI } from "web-ifc/web-ifc-api";
+import{ IfcAPI, IFCWALLSTANDARDCASE } from "web-ifc/web-ifc-api";
 
-console.log(IfcAPI)
+const ifcAPI = new IfcAPI()
+ifcAPI.Init();
+
+console.log("test");
+const button = document.getElementById("input-button");
+const input = document.getElementById("input-element");
+const resultContainer = document.getElementById("content");
+
+button.onclick = () => {
+    input.click()
+}
+
+input.onchange = (changed) => {
+    const reader = new FileReader() ;
+    reader.onload = () => loadIfc(reader.result);
+    reader.readAsText(changed.target.files[0]);
+}
+
+// function loadIfc(ifcData) {
+//     const modelID = ifcAPI.OpenModel(ifcData);
+
+//     const walls = ifcAPI.GetLineIDsWithType(modelID, IFCWALLSTANDARDCASE);
+
+//     const size = walls.size();
+//     for(let i = 0; i < size; i++) {
+//         const wall = walls.get(i);
+//         const wallProps = ifcAPI.GetLine(modelID, wall);
+//         console.log(wallProps);
+//     }
+
+// }
+
+function loadIfc(ifcData) {
+    const modelID = ifcAPI.OpenModel(ifcData);
+
+    const walls = ifcAPI.GetLineIDsWithType(modelID, IFCWALLSTANDARDCASE);
+    
+    const firstWall = walls.get(0);
+    const wallProps = ifcAPI.GetLine(modelID, firstWall);
+
+    const result = JSON.stringify(wallProps, undefined, 2);
+    resultContainer.textContent = result
+
+}
+    
